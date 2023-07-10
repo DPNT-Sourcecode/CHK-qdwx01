59fb2ab61e46436f2ea791fecaa592fddd1cf2a0
sbx_user1051 <sbx_user1051@169.254.16.117>
2023-07-10 13:12:16 +0000

    Mon Jul 10 13:12:16 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -7,12 +7,16 @@ import java.util.stream.Collectors;
 
 public class CheckoutSolution {
     public static final Map<Character, Integer> INDIVIDUAL_PRICES = new HashMap<>();
-    public static final int SPECIAL_OFFER_A_FOR_3 = 130;
+    public static final Pair<Integer, Integer> SPECIAL_OFFER_A_FOR_3 = new Pair(3, 130);
     public static final int SPECIAL_OFFER_A_FOR_5 = 200;
     public static final int SPECIAL_OFFER_B_FOR_2 = 45;
     public static final int SPECIAL_OFFER_H_FOR_5 = 45;
-    public static final int SPACIAL_OFFER_H_FOR_10 = 80;
-    public static final int SPACIAL_OFFER_K_FOR_2 = 150;
+    public static final int SPECIAL_OFFER_H_FOR_10 = 80;
+    public static final int SPECIAL_OFFER_K_FOR_2 = 150;
+    public static final int SPECIAL_OFFER_P_FOR_5 = 200;
+    public static final int SPECIAL_OFFER_Q_FOR_3 = 80;
+    public static final int SPECIAL_OFFER_V_FOR_2 = 90;
+    public static final int SPECIAL_OFFER_V_FOR_3 = 130;
 
     static {
         INDIVIDUAL_PRICES.put('A', 50);
@@ -25,28 +29,23 @@ public class CheckoutSolution {
         INDIVIDUAL_PRICES.put('H', 10);
         INDIVIDUAL_PRICES.put('I', 35);
         INDIVIDUAL_PRICES.put('J', 60);
-        
+        INDIVIDUAL_PRICES.put('K', 80);
+        INDIVIDUAL_PRICES.put('L', 90);
+        INDIVIDUAL_PRICES.put('M', 15);
+        INDIVIDUAL_PRICES.put('N', 40);
+        INDIVIDUAL_PRICES.put('O', 10);
+        INDIVIDUAL_PRICES.put('P', 50);
+        INDIVIDUAL_PRICES.put('Q', 30);
+        INDIVIDUAL_PRICES.put('R', 50);
+        INDIVIDUAL_PRICES.put('S', 30);
+        INDIVIDUAL_PRICES.put('T', 20);
+        INDIVIDUAL_PRICES.put('U', 40);
+        INDIVIDUAL_PRICES.put('V', 50);
+        INDIVIDUAL_PRICES.put('W', 20);
+        INDIVIDUAL_PRICES.put('X', 90);
+        INDIVIDUAL_PRICES.put('Y', 10);
+        INDIVIDUAL_PRICES.put('Z', 50);
     }
-
-
-    /*
-    | K    | 80    | 2K for 150             |
-    | L    | 90    |                        |
-    | M    | 15    |                        |
-    | N    | 40    | 3N get one M free      |
-    | O    | 10    |                        |
-    | P    | 50    | 5P for 200             |
-    | Q    | 30    | 3Q for 80              |
-    | R    | 50    | 3R get one Q free      |
-    | S    | 30    |                        |
-    | T    | 20    |                        |
-    | U    | 40    | 3U get one U free      |
-    | V    | 50    | 2V for 90, 3V for 130  |
-    | W    | 20    |                        |
-    | X    | 90    |                        |
-    | Y    | 10    |                        |
-    | Z    | 50    |
-     */
 
     private Integer calculateTotalPrice(char item, Long count, Integer individualPrice) {
         switch (item) {
@@ -120,4 +119,5 @@ public class CheckoutSolution {
                 .sum();
     }
 }
+
 
