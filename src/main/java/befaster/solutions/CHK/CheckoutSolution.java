001afd78eb9ecbda0aa716bb65e38b70e695fd96
sbx_user1051 <sbx_user1051@169.254.16.117>
2023-07-10 13:45:16 +0000

    Mon Jul 10 13:45:16 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -8,16 +8,12 @@ import java.util.stream.Collectors;
 public class CheckoutSolution {
     public static final Map<Character, Integer> INDIVIDUAL_PRICES = new HashMap<>();
     public static final DoubleOffer SPECIAL_OFFER_A = new DoubleOffer(5, 200, 3, 130);
-    public static final Offer SPECIAL_OFFER_A_FOR_3 = new Offer(3, 130);
-    public static final Offer SPECIAL_OFFER_A_FOR_5 = new Offer(5, 200);
     public static final Offer SPECIAL_OFFER_B = new Offer(2, 45);
-    public static final Offer SPECIAL_OFFER_H_FOR_5 = new Offer(5, 45);
-    public static final Offer SPECIAL_OFFER_H_FOR_10 = new Offer(10, 80);
+    public static final DoubleOffer SPECIAL_OFFER_H = new DoubleOffer(10, 80, 5, 45);
     public static final Offer SPECIAL_OFFER_K = new Offer(2, 150);
     public static final Offer SPECIAL_OFFER_P = new Offer(5, 200);
     public static final Offer SPECIAL_OFFER_Q = new Offer(3, 80);
-    public static final Offer SPECIAL_OFFER_V_FOR_2 = new Offer(2, 90);
-    public static final Offer SPECIAL_OFFER_V_FOR_3 = new Offer(3, 130);
+    public static final DoubleOffer SPECIAL_OFFER_V = new DoubleOffer(3, 130, 2, 90);
 
     static {
         INDIVIDUAL_PRICES.put('A', 50);
@@ -55,9 +51,7 @@ public class CheckoutSolution {
             case 'B':
                 return SPECIAL_OFFER_B.calculateOffer(count, individualPrice);
             case 'H':
-                return (int) (count / SPECIAL_OFFER_H_FOR_10.getNumber() * SPECIAL_OFFER_H_FOR_10.getPrice()
-                        + count %  SPECIAL_OFFER_H_FOR_10.getNumber() / SPECIAL_OFFER_H_FOR_5.getNumber() * SPECIAL_OFFER_H_FOR_5.getPrice()
-                        + (count % SPECIAL_OFFER_H_FOR_10.getNumber()) % SPECIAL_OFFER_H_FOR_5.getNumber() * individualPrice);
+                return SPECIAL_OFFER_H.calculateOffer(count, individualPrice);
             case 'K':
                 return SPECIAL_OFFER_K.calculateOffer(count, individualPrice);
             case 'P':
@@ -65,9 +59,7 @@ public class CheckoutSolution {
             case 'Q':
                 return SPECIAL_OFFER_Q.calculateOffer(count, individualPrice);
             case 'V':
-                return (int) (count / SPECIAL_OFFER_V_FOR_3.getNumber() * SPECIAL_OFFER_V_FOR_3.getPrice()
-                        + count %  SPECIAL_OFFER_V_FOR_3.getNumber() / SPECIAL_OFFER_V_FOR_2.getNumber() * SPECIAL_OFFER_V_FOR_2.getPrice()
-                        + (count % SPECIAL_OFFER_V_FOR_3.getNumber()) % SPECIAL_OFFER_V_FOR_2.getNumber() * individualPrice);
+                return SPECIAL_OFFER_V.calculateOffer(count, individualPrice);
             default:
                 return (int) (count * individualPrice);
         }
@@ -132,48 +124,21 @@ public class CheckoutSolution {
                 .sum();
     }
 
-    private static class Offer {
-        private final int number;
-        private final int price;
-
-        public Offer(int number, int price) {
-            this.number = number;
-            this.price = price;
-        }
-
-        public int getNumber() {
-            return number;
-        }
-
-        public int getPrice() {
-            return price;
-        }
-
+    private record Offer(int number, int price) {
         public int calculateOffer(long count, int individualPrice) {
-            return (int) (count / this.number * this.price + count % this.number * individualPrice);
-        }
-    }
-
-    private static class DoubleOffer {
-        private final int largeNumber;
-        private final int largePrice;
-        private final int smallNumber;
-        private final int smallPrice;
-
-        public DoubleOffer(int largeNumber, int largePrice, int smallNumber, int smallPrice) {
-            this.largeNumber = largeNumber;
-            this.largePrice = largePrice;
-            this.smallNumber = smallNumber;
-            this.smallPrice = smallPrice;
+                return (int) (count / this.number * this.price + count % this.number * individualPrice);
+            }
         }
 
+    private record DoubleOffer(int largeNumber, int largePrice, int smallNumber, int smallPrice) {
         public int calculateOffer(long count, int individualPrice) {
-            return (int) (count / largeNumber * largePrice
-                    + count % largeNumber / smallNumber * smallPrice
-                    + (count % largeNumber) % smallNumber * individualPrice);
+                return (int) (count / largeNumber * largePrice
+                        + count % largeNumber / smallNumber * smallPrice
+                        + (count % largeNumber) % smallNumber * individualPrice);
+            }
         }
-    }
 }
+
 
 
 
