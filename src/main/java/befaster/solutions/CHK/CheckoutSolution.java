1d57462f5a61dd8f066041190ad7b3469622565a
sbx_user1051 <sbx_user1051@169.254.16.117>
2023-07-10 13:03:16 +0000

    Mon Jul 10 13:03:16 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -10,6 +10,9 @@ public class CheckoutSolution {
     public static final int SPECIAL_OFFER_A_FOR_3 = 130;
     public static final int SPECIAL_OFFER_A_FOR_5 = 200;
     public static final int SPECIAL_OFFER_B_FOR_2 = 45;
+    public static final int SPECIAL_OFFER_H_FOR_5 = 45;
+    public static final int SPACIAL_OFFER_H_FOR_10 = 80;
+    public static final int SPACIAL_OFFER_K_FOR_2 = 150;
 
     static {
         INDIVIDUAL_PRICES.put('A', 50);
@@ -18,7 +21,32 @@ public class CheckoutSolution {
         INDIVIDUAL_PRICES.put('D', 15);
         INDIVIDUAL_PRICES.put('E', 40);
         INDIVIDUAL_PRICES.put('F', 10);
+        INDIVIDUAL_PRICES.put('G', 20);
+        INDIVIDUAL_PRICES.put('H', 10);
+        INDIVIDUAL_PRICES.put('I', 35);
+        INDIVIDUAL_PRICES.put('J', 60);
+        
     }
+
+
+    /*
+    | K    | 80    | 2K for 150             |
+    | L    | 90    |                        |
+    | M    | 15    |                        |
+    | N    | 40    | 3N get one M free      |
+    | O    | 10    |                        |
+    | P    | 50    | 5P for 200             |
+    | Q    | 30    | 3Q for 80              |
+    | R    | 50    | 3R get one Q free      |
+    | S    | 30    |                        |
+    | T    | 20    |                        |
+    | U    | 40    | 3U get one U free      |
+    | V    | 50    | 2V for 90, 3V for 130  |
+    | W    | 20    |                        |
+    | X    | 90    |                        |
+    | Y    | 10    |                        |
+    | Z    | 50    |
+     */
 
     private Integer calculateTotalPrice(char item, Long count, Integer individualPrice) {
         switch (item) {
@@ -92,3 +120,4 @@ public class CheckoutSolution {
                 .sum();
     }
 }
+
