e9f04dc3ca55dbf97c5742afe3e4e0892dfa565d
sbx_user1051 <sbx_user1051@169.254.16.117>
2023-07-10 11:46:48 +0000

    Mon Jul 10 11:46:48 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -27,7 +27,7 @@ public class CheckoutSolution {
             case 'B':
                 return (int) (count / 2 * SPECIAL_OFFER_B_FOR_2 + count % 2 * individualPrice);
             case 'E':
-                long numberOfBs = productCount.get('B');
+                long numberOfBs = productCount.get(Character.valueOf('B'));
                 long numberOfFreeBs = count / 2;
                 long discount = numberOfBs <= numberOfFreeBs
                         ? calculateTotalPrice('B', numberOfBs, INDIVIDUAL_PRICES.get('B'), productCount)
@@ -76,3 +76,4 @@ public class CheckoutSolution {
                 .sum();
     }
 }
+
